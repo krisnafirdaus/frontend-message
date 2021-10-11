94c260224237f6e404cc1417362422ba58f804b8
krisnafirdaus <krisnafirdaus@gmail.com>
2021-10-11 10:34:44 +0700

    fix form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 import "./Login.css";
 
 export default function SignIn() {
-  const [name, setName] = useState("");
-  const [room, setRoom] = useState("");
+    const [name, setName] = useState("");
+    const [room, setRoom] = useState("");
 
-  return (
-    <div className="joinOuterContainer">
-      <div className="joinInnerContainer">
-        <h1 className="heading">Masuk</h1>
-        {/* <div className="form">
+    return (
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Masuk</h1>
+                {/* <div className="form">
           <input
             placeholder="Name"
             className="joinInput"
@@ -29,41 +29,43 @@ export default function SignIn() {
             Email
           </label>
         </div> */}
-        <div class="form">
-          <input
-            type="text"
-            id="name"
-            class="form__input"
-            autocomplete="off"
-            placeholder=" "
-            onChange={(event) => setName(event.target.value)}
-          />
-          <label for="name" class="form__label">
-            Name
-          </label>
+                <div class="form">
+                    <input
+                        type="text"
+                        id="name"
+                        class="form__input"
+                        autocomplete="off"
+                        placeholder=" "
+                        onChange={(event) => setName(event.target.value)}
+                    />
+                    <label for="name" class="form__label">
+                        Name
+                    </label>
+                </div>
+                <div class="form mt-6">
+                    <input
+                        type="text"
+                        id="room"
+                        class="form__input"
+                        autocomplete="off"
+                        placeholder=" "
+                        onChange={(event) => setRoom(event.target.value)}
+                    />
+                    <label for="room" class="form__label">
+                        ID Room
+                    </label>
+                </div>
+                <Link
+                    onClick={(e) =>
+                        !name || !room ? e.preventDefault() : null
+                    }
+                    to={`/chat?name=${name}&room=${room}`}
+                >
+                    <button className="style-1 mt-20" type="submit">
+                        Masuk
+                    </button>
+                </Link>
+            </div>
         </div>
-        <div class="form mt-6">
-          <input
-            type="text"
-            id="room"
-            class="form__input"
-            autocomplete="off"
-            placeholder=" "
-            onChange={(event) => setRoom(event.target.value)}
-          />
-          <label for="room" class="form__label">
-            Room
-          </label>
-        </div>
-        <Link
-          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
-          to={`/chat?name=${name}&room=${room}`}
-        >
-            <button className="style-1 mt-20" type="submit">
-              Masuk
-            </button>
-        </Link>
-      </div>
-    </div>
-  );
+    );
 }
